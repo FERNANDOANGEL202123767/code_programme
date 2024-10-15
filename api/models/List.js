fd5bc5d7965afd18d70770bbd27691b6ff6758c8
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-15 13:14:05 -0600

    Update List.js

--- api/models/List.js
+++ api/models/List.js
@@ -1,1 +1,17 @@
+/**
+ * modelo lista.js
+ * @author Jonathan Hernandez
+ */
+const mongoose = require("mongoose");
 
+const ListSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    type: { type: String },
+    genre: { type: String },
+    content:{type:Array}
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("List", ListSchema);
